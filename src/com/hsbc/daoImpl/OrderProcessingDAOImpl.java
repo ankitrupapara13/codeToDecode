10bec60c4ad469c0c95ff2c4a2da1c79b9fb64bd
unknown <ujjwalmishra21@gmail.com>
2020-10-11 18:32:18 +0530

    changes

--- src/com/hsbc/daoImpl/OrderProcessingDAOImpl.java
+++ src/com/hsbc/daoImpl/OrderProcessingDAOImpl.java
@@ -39,7 +39,7 @@ public class OrderProcessingDAOImpl implements OrderProcessingDAO {
 		try {
 			OrderProcessingDAO emImpl = OrderProcessingDAOImpl.getInstance();
 			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-			con = DriverManager.getConnection("jdbc:derby:C:\\Users\\Ankit\\\\MyDB;create=true", "admin", "derby");
+			con = DriverManager.getConnection("jdbc:derby:C:\\Users\\Ankit\\MyDB;create=true", "admin", "derby");
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
